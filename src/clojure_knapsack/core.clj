f4420144fa50091f1dd8086dfd0397e8dd54fff1
Ben Nash <benash@umich.edu>
2013-03-30 00:32:10 -0400

    Read file into map

--- src/clojure_knapsack/core.clj
+++ src/clojure_knapsack/core.clj
@@ -1,6 +1,29 @@
-(ns clojure-knapsack.core)
+(ns clojure-knapsack.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn read-lines
+  "Read lines in filename into sequence"
+  [filename]
+  (str/split-lines (slurp filename)))
+
+(defn make-max-weight [line]
+  (Integer/parseInt line))
+
+(defn parse-doll
+  "Make a map from the name, weight, and value strings of a doll"
+  [[name weight value]]
+  (zipmap [:name :weight :value]
+          [name (Integer/parseInt weight) (Integer/parseInt value)]))
+
+(defn make-doll [line]
+  (parse-doll (str/split line #"\s+")))
+
+(defn parse-input [filename]
+  (let [[weight-line & doll-lines] (read-lines filename)]
+    {:max-weight (make-max-weight weight-line)
+     :dolls (map make-doll doll-lines)}))
+
+(defn build-knapsack-table [scenario]
+  (repeat (+ 1 (:max-weight scenario)) 0))
+
+;(print-result (build-knapsack-table (parse-input file)))
--- test/clojure_knapsack/core_test.clj
+++ test/clojure_knapsack/core_test.clj
@@ -2,6 +2,29 @@
   (:use clojure.test
         clojure-knapsack.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defmacro file [] "test/small.txt")
+(defmacro doll-map [] {:value 150, :name "luke", :weight 9})
+
+(deftest read-lines-test
+  (testing "Check read-lines"
+    (is (= '("100" "luke        9   150" "anthony    13    35")
+           (read-lines (file))))))
+
+(deftest make-max-weight-test
+  (testing "Check make-max-weight"
+    (is (= 400 (make-max-weight "400")))))
+
+(deftest parse-doll-test
+  (testing "Check parse-doll"
+    (is (= (doll-map) (parse-doll ["luke" "9" "150"])))))
+
+(deftest make-doll-test
+  (testing "Check make-doll"
+    (is (= (doll-map) (make-doll "luke  9 150")))))
+
+(deftest parse-input-test
+  (testing "Check parse-file"
+    (is (= {:max-weight 100, :dolls '({:value 150, :weight 9,  :name "luke"}
+                                      {:value 35,  :weight 13, :name "anthony"})}
+           (parse-input (file))))))
+
